4b1cd7108170fdb5c56b67b61023a56875dd4632
Joe Sycalik <sycalik@gmail.com>
2019-01-26 12:38:41 -0500

    Defending Player Initial Setup
    - Defending players can send input to an object with the FallingObject tag in order to move it
    - The camera will now follow an object tagged with the FallingObject tag
    - The wrappableObject script can be attached to any object that needs the ability to wrap around the screen

---
+++ Project Splatterhouse/Assets/Scripts/DefendingPlayer.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+
+public class DefendingPlayer : MonoBehaviour
+{
+    // 0.3f with position movement
+    // 10 with physics movement
+    [SerializeField]
+    float _horizontalSpeed;
+
+    GameObject _fallingUnit;
+    Rigidbody _unitRigidbody;
+
+    #region Unity Core Methods
+
+    private void Start()
+    {
+        _fallingUnit = GameObject.FindGameObjectWithTag("FallingObject");
+        _unitRigidbody = _fallingUnit.GetComponent<Rigidbody>();
+    }
+    private void Update()
+    {
+        CheckInput();
+    }
+    #endregion
+
+    #region Input Handling
+    void CheckInput()
+    {
+        float _moveHorizontal = Input.GetAxis("Horizontal");
+        SendInput(_moveHorizontal);
+    }
+
+    void SendInput(float _moveHorizontal)
+    {
+        Vector3 _movement = new Vector3(_moveHorizontal, 0.0f, 0.0f);
+
+        _fallingUnit.transform.localPosition += _movement * _horizontalSpeed;
+
+        // Optional other form of movement using physics instead of direct position adjustment
+        //_unitRigidbody.AddForce(_movement * _horizontalSpeed);
+    }
+    #endregion
+}
---
+++ Project Splatterhouse/Assets/Scripts/WrappableObject.cs
@@ -0,0 +1,71 @@
+﻿using UnityEngine;
+
+public class WrappableObject : MonoBehaviour
+{
+    Renderer _renderer;
+    bool _isWrappingX, _isWrappingY;
+    Camera _cam;
+
+    #region Unity Core Methods
+    void Start()
+    {
+        _renderer = GetComponent<Renderer>();
+        _cam = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Camera>();
+    }
+
+    private void Update()
+    {
+        ScreenWrap();
+    }
+    #endregion
+
+    #region Object Wrapping
+    bool CheckRenderer()
+    {
+        // If at least one render is visible, return true
+        if (_renderer.isVisible)
+        {
+            return true;
+        }
+
+        // Otherwise, the object is invisible
+        return false;
+    }
+
+    void ScreenWrap()
+    {
+        bool _isVisible = CheckRenderer();
+
+        if (_isVisible)
+        {
+            _isWrappingX = false;
+            _isWrappingY = false;
+            return;
+        }
+
+        if (_isWrappingX && _isWrappingY)
+        {
+            return;
+        }
+
+        Vector3 _viewportPosition = _cam.WorldToViewportPoint(transform.position);
+        Vector3 _newPosition = transform.position;
+
+        if (!_isWrappingX && (_viewportPosition.x > 1 || _viewportPosition.x < 0))
+        {
+            _newPosition.x = -_newPosition.x;
+
+            _isWrappingX = true;
+        }
+
+        if (!_isWrappingY && (_viewportPosition.y > 1 || _viewportPosition.y < 0))
+        {
+            _newPosition.y = -_newPosition.y;
+
+            _isWrappingY = true;
+        }
+
+        transform.position = _newPosition;
+    }
+    #endregion
+}

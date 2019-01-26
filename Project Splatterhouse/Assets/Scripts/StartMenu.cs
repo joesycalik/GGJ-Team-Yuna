40bb438da5096de9a4b6f9e496eb1233d7df9e30
Rebecca <rebeccabushko@gmail.com>
2019-01-26 16:11:36 -0500

    Some error checking and hooking up UI

--- Project Splatterhouse/Assets/Scripts/StartMenu.cs
+++ Project Splatterhouse/Assets/Scripts/StartMenu.cs
@@ -9,17 +9,21 @@ using UnityEngine.SceneManagement;
 public class StartMenu : MonoBehaviour
 {
     private const int MAX_PLAYERS = 4;
+    private const int MAX_TEAMS = 2;
 
     private int[] _teamChoices;
-    public GameObject airConsoleGO;
+
+    private List<PlayerButtonController> _playerButtons;
+
+    public GameObject panel;
+    public Text title;
 
     void Awake() 
     {
-        AirConsole.instance.onConnect += OnConnect;
-		AirConsole.instance.onMessage += OnMessage;
+        AddCallbacks();
 
-        Object.DontDestroyOnLoad(airConsoleGO);
         _teamChoices = new int[MAX_PLAYERS] {0, 0, 0, 0};
+        _playerButtons = new List<PlayerButtonController>(panel.GetComponentsInChildren<PlayerButtonController>(true));
 	}
 
     void OnConnect (int deviceId) 
@@ -29,34 +33,73 @@ public class StartMenu : MonoBehaviour
         {
 			AirConsole.instance.SetActivePlayers(4);
             Debug.LogWarning("Ready!");
+
+            foreach (PlayerButtonController pbc in _playerButtons)
+            {
+                pbc.gameObject.SetActive(true);
+            }
+
+            title.text = "Hold to Join!";
 		}
 	}
 
     void OnMessage(int deviceId, JToken data) 
     {
+        if (_teamChoices == null)
+        {
+            return;
+        }
+
 		int activePlayer = AirConsole.instance.ConvertDeviceIdToPlayerNumber(deviceId);
         int teamChoice = (int)data["join"];
 
         Debug.LogWarning("Player: " + activePlayer + " Team: " + teamChoice);
 
-        _teamChoices[activePlayer] = teamChoice;
-	}		
+        if (CanJoinTeam(teamChoice))
+        {
+            _teamChoices[activePlayer] = teamChoice;
 
-    // Update is called once per frame
-    void Update()
-    {
+            if (teamChoice > 0)
+            {
+                _playerButtons[activePlayer].Join();
+            }
+            else
+            {
+                _playerButtons[activePlayer].UnJoin();
+            }
+        }
+
         if (ShouldStart())
         {
             RemoveCallbacks();
             SceneManager.LoadScene("GameScene");
         }
-    }
+	}
 
     private bool ShouldStart()
     {
-        if (_teamChoices == null)
+        int team1Count = 0;
+        int team2Count = 0;
+        foreach (int teamNum in _teamChoices)
         {
-            return false;
+            if (teamNum == 1)
+            {
+                team1Count++;
+            }
+            else if (teamNum == 2)
+            {
+                team2Count++;
+            }
+        }
+
+        return team1Count == 2 && team2Count == 2;
+    }
+
+    private bool CanJoinTeam(int teamChoice)
+    {
+        if (teamChoice == 0)
+        {
+            return true;
         }
 
         int team1Count = 0;
@@ -73,7 +116,22 @@ public class StartMenu : MonoBehaviour
             }
         }
 
-        return team1Count == 2 && team2Count == 2;
+        if (teamChoice == 1)
+        {
+            return team1Count < 2;
+        }
+        else if (teamChoice == 2)
+        {
+            return team2Count < 2;
+        }
+
+        return false;
+    }
+
+    private void AddCallbacks()
+    {
+        AirConsole.instance.onConnect += OnConnect;
+		AirConsole.instance.onMessage += OnMessage;
     }
 
     private void RemoveCallbacks()
